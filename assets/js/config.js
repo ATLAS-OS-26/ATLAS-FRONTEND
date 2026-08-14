95df1afb937b891e14da7b17e49092bfc9e191e1
ATLAS-OS-26 <aitingaso2003@gmail.com>
2026-08-14 12:04:24 -0700

    Initial commit

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+const APP_CONFIG = {
+    systemName: "ATLAS OS",
+    agencyName: "Aerix Travel",
+    agencyLogo: "assets/images/atlas-logo.png",
+    version: "1.0.0",
+    language: "ar",
+    currency: "SAR"
+};
